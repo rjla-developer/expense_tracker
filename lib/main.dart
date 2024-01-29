33d9183e6d5f05217af7916e57a1e680a2581cb8
RJDalefon <rjluengas@dalefon.mx>
2024-01-29 09:28:15 -0600

    Base del proyecto flutter

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//Widgets:
+import 'package:expense_tracker/expenses.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Flutter Tutorial',
+    home: Expenses(),
+  ));
+}
