28909ffa5c6ff0d630f3645e3d59083f85f2c3b9
RJDalefon <rjluengas@dalefon.mx>
2024-01-30 09:47:25 -0600

    Avances en el proyecto

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+//Class:
+import 'package:expense_tracker/models/expense.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -8,6 +11,18 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: "Flutter Course",
+        amount: 15.68,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: "Groceries",
+        amount: 50.00,
+        date: DateTime.now(),
+        category: Category.food),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
